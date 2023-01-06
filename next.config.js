46403e98b8fa172de3a5e01d33aaf9d6ab3c9062
Petr Kočí <petr.koci@samizdat.cz>
2023-01-06 19:09:49 +0100

    cards layout

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   basePath: "/prez-anketa",
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "data.irozhlas.cz",
+        port: "",
+        pathname: "/prez-anketa/img/**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Image from "next/image";
 import { Reorder } from "framer-motion";
 import { useState } from "react";
 import candidates from "../data/candidates.json";
@@ -25,7 +26,7 @@ export default function Home() {
         </h2>
         <div className="px-1 pt-3">
           <Reorder.Group
-            className="space-y-3"
+            className="space-y-2"
             as="ol"
             role="list"
             axis="y"
@@ -36,9 +37,26 @@ export default function Home() {
               <Reorder.Item
                 key={item.id}
                 value={item}
-                className="cursor-grab overflow-hidden rounded-md bg-white px-6 py-4 shadow active:cursor-grabbing"
+                className="relative flex cursor-grab items-center space-x-3 overflow-hidden rounded-lg border border-gray-300 bg-white px-3 py-3 shadow-md focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400 active:cursor-grabbing"
               >
-                {`${items.indexOf(item) + 1}. ${item.name}`}
+                <div className="flex-shrink-0">
+                  <Image
+                    src={`https://data.irozhlas.cz/prez-anketa/img/${item.id}.png`}
+                    alt={item.name}
+                    width={50}
+                    height={50}
+                    className="rounded-full"
+                    draggable="false"
+                  ></Image>
+                </div>
+                <div className="min-w-0 flex-1">
+                  <span className="absolute inset-0" aria-hidden="true" />
+                  <p className="text-sm font-medium text-gray-900">{`${
+                    items.indexOf(item) + 1
+                  }. ${item.name}`}</p>
+                  <p className="truncate text-sm text-gray-500">{`${item.age} let, ${item.city}`}</p>
+                  <p className="truncate text-sm text-gray-500">{item.job}</p>
+                </div>
               </Reorder.Item>
             ))}
           </Reorder.Group>
